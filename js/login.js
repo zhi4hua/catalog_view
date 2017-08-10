0f50389c4946df78086e3a1997c769dcc43b6cd2
zhi4hua <zhi4hua@yeah.net>
2017-08-10 15:51:29 +0800

    加强登录页面

--- js/login.js
+++ js/login.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
     $('#login').hide().fadeIn();
-    $('#loading').addClass('test');
     $('#login :submit').click(function() {
         $(this).attr('disabled', 'disabled').addClass('disabled');
+
+        // detach loading
+        var loadingNode = $('#loading').detach();
+        $('#login').append(loadingNode).children("#loading").removeClass('template').automaticVerticalCentering();
+
         return false;
     })
 });
+
+// Automatic vertical centering
+// 自动竖直居中
+$.fn.automaticVerticalCentering = function() {
+    this.css({
+        'height': this.height() + 'px',
+        'line-height' : this.height() + 'px'
+    });
+}
--- login.php
+++ login.php
@@ -1,5 +1,8 @@
 <?php
     require_once('./includes/website_data.php');
+    // import database
+    // require_once('./includes/database.php');
+    // return false;
     ob_start();
 
     // Verify that you are already logged in
@@ -7,17 +10,17 @@
     session_start();
     if ($_SESSION['userName'])
     {
-        echo '<h1>'.$_SESSION['userName'].'</h1>';
+        // echo '<h1>'.$_SESSION['userName'].'</h1>';
     }
 
     // seting web title
     $GLOBALS['TEMPLATE']['title'] = 'login';
 ?>
-    <div class="login_frame theme_color">
+    <div class="login_frame gray">
         <div class="middle">
             <div class="login" id="login">
-                <div class="logo">
-                    logo
+                <div class="logo theme_color">
+                    <i class="fa fa-cloud fa-3x fa-fw animate-text-hover" aria-hidden="true"></i>
                 </div>
                 <h1>Sign in</h1>
                 <h3>Use your account.</h3>
@@ -30,22 +33,38 @@
                     <br />
                     <input type="submit" class="wide theme_background_color hover_theme_color white_color center" value="Next">
                     <div>
-                        <span>No account?</span><a href="#">Create one!</a>
+                        <span>No account?</span><a class="theme_color" href="#">Create one!</a>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+
+    <!-- loading frame -->
+    <link rel="stylesheet" type="text/css" href="./css/font-awesome.css" />
+    <!-- loading icon -->
+    <div id="loading" class="loading theme_color template">
+        <i class="fa fa-circle-o-notch fa-spin fa-4x fa-fw"></i>
+        <span class="sr-only">Loading...</span>
+        Loading...
+    </div>
+    <script>
+        // centered icon
+        // 居中图标
+        // alert('to this !');
+    </script>
+
     
 <?php
     $GLOBALS['TEMPLATE']['content'] = ob_get_contents();
-    $stylesArray = array('https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css', './css/global.css', './css/login.css');
+    $stylesArray = array('./css/global.css', './css/login.css');
     $GLOBALS['TEMPLATE']['styles'] = $stylesArray;
     ob_end_clean();
 ?>
 <?php
     ob_start();
 ?>
+    <script type="text/javascript" src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script type="text/javascript" src="./js/login.js"></script>
 <?php 
     $GLOBALS['TEMPLATE']['scriptStream'] = ob_get_contents();
