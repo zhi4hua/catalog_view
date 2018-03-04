0444eaf82cadf2edd2eb5e085f14e3e2b2a0beb9
zhi4hua <15179872521@163.com>
2018-03-05 06:44:21 +0800

    更新首页, Update page index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php     
-    header('Location:./template-page.html'); 
+    // include website data
+    require_once('./includes/website_data.php');
+    session_start();
+
+    // Test romate database
+    header("Location:". WEBSITE_ADDR. '/Test_menu_bootstrap.php');
+
+    // if not logged in
+    // 如果没有登录
+    // if (!isset($_SESSION['userId']) || empty($_SESSION['userId'])){
+    //     header('Location:'. LOGIN_PATH);
+    // }
+    // else {
+    //     header('Location:'.WEBSITE_ADDR.'/catalog_view.php');
+    // }
+    session_write_close();
 ?>
